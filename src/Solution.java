9e4f9e6ec4ca38fca56199f8f29c8890350cca48
NehaAnnPhilip <nehaphilip0205@gmail.com>
2026-04-10 19:43:37 +0530

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,35 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Read a single line of text
-        String line = scanner.nextLine();
-        
-        // Split the text into individual words
-        String[] words = line.split(" ");
-        
-        // Create a HashMap to store frequencies
-        HashMap<String, Integer> freqMap = new HashMap<>();
-        
-        // Count frequencies
-        for (String word : words) {
-            if (freqMap.containsKey(word)) {
-                freqMap.put(word, freqMap.get(word) + 1);
-            } else {
-                freqMap.put(word, 1);
+        if (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] words = line.split("\\s+");
+            java.util.HashMap<String, Integer> map = new java.util.HashMap<>();
+            
+            for (String word : words) {
+                if (!word.isEmpty()) {
+                    map.put(word, map.getOrDefault(word, 0) + 1);
+                }
             }
+            
+            // Sort by frequency (descending) then alphabetically
+            java.util.List<java.util.Map.Entry<String, Integer>> list = new java.util.ArrayList<>(map.entrySet());
+            list.sort((a, b) -> {
+                int freqCompare = b.getValue().compareTo(a.getValue());
+                if (freqCompare != 0) return freqCompare;
+                return a.getKey().compareTo(b.getKey());
+            });
+            
+            for (java.util.Map.Entry<String, Integer> entry : list) {
+                System.out.print(entry.getKey() + ": " + entry.getValue() + " ");
+            }
+            System.out.println();
         }
-        
-        // Print results
-        for (Map.Entry<String, Integer> entry : freqMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
-        
         scanner.close();
     }
 }
